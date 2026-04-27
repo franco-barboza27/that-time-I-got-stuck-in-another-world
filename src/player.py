eea8210b510ec1135c4dd4cedeee43bdf8ca03f8
HunterWildClaw <nathan.hasson@ucas-edu.net>
2026-04-27 10:34:23 -0600

    worked on player hitbox

--- src/player.py
+++ src/player.py
@@ -1,4 +1,9 @@
+import pygame
+
+pygame.init()
+
 class Player:
-    def movement_inputs():
-        def __init__(self):
-            
+    def __init__(self):
+        self.rect = pygame.Rect(100, 300, 40, 80)
+        self.vel_y = 0
+        self.on_ground = False
