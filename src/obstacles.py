f78aa0e290ad1f8a6f741d9d709e0aafd9e4ada1
franco.barboza27 <franco.barboza@ucas-edu.net>
2026-04-28 11:47:11 -0600

    The bird works without gravity or air checks, also doesn't kill

--- src/obstacles.py
+++ src/obstacles.py
@@ -15,6 +15,10 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Test")
 clock = pygame.time.Clock()
 
+player = pygame.Rect((300, 250, 50, 50))
+
+bird = pygame.Rect((1000, 1000, 50, 50))
+
 class block:
     def __init__(self, danger, size, spawndist, coords, sprite=None):
         danger = danger
@@ -22,33 +26,57 @@ class block:
         spawndist = spawndist
         coords = coords
     
-    def mover(self, x, y):
-        self.coords[0] += x
-        self.coords[1] += y
+def mover(bird, x, y):
+        bird.move_ip(x, y)
     
-    def birdmovement(self, player):
-        if player.state=="air" and self.danger=="bird":
-            if player.coords[0] >= self.coords[0]:
-                xdif = True
-            else:
-                xdif = False
-            if player.coords[1] >= self.coords[1]:
-                ydif = True
-            else:
-                ydif = False
+def birdmovement(bird, player):
+    #if player.state=="air" and bird.danger=="bird":
+    if player.left >= bird.left:
+        xdif = True
+    else:
+        xdif = False
+    if player.top >= bird.top:
+        ydif = True
+    else:
+        ydif = False
 
-            if xdif == True:
-                xdir = 1
-            else:
-                xdir = -1
-            
-            if ydif == True:
-                ydir = 1
-            else:
-                ydir = -1
-            
-        self.mover(10*xdir, 10*ydir)
+    if xdif == True:
+        xdir = 1
+    else:
+        xdir = -1
+    
+    if ydif == True:
+        ydir = 1
+    else:
+        ydir = -1
+        
+    mover(bird, xdir, ydir)
 
+run = True
+while run:
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (255, 0, 0), player)
+    pygame.draw.rect(screen, (66,228, 87), bird)
 
-c = pygame.Rect(block("safe", (100,100), 100, [100, 0]))
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w] == True:
+        player.move_ip(0, -2)
+    
+    if key[pygame.K_a] == True:
+        player.move_ip(-2, 0)
+    
+    if key[pygame.K_s] == True:
+        player.move_ip(0, 2)
+    
+    if key[pygame.K_d] == True:
+        player.move_ip(2, 0)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
+    birdmovement(bird, player)
+    
+    pygame.display.update()
+
+pygame.quit()
