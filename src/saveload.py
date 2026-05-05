30e4ad00e1ba9da3464a47f67db03622a8536dc9
franco.barboza27 <franco.barboza@ucas-edu.net>
2026-05-05 10:50:59 -0600

    started the saving, and started combined/modularized the birds and gameloop

--- src/saveload.py
+++ src/saveload.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import pathlib
 
 def load(savepath):
     pd.read_csv(savepath)
 
 def levelload(savepath):
-    pd.read_csv(savepath)
+    basepath = pathlib.Path(__file__).resolve().parent
+    filepath = basepath.parent / 'docs' / savepath
+
+    blocks = pd.read_csv(filepath)
+
+    
+
+    return blocks
